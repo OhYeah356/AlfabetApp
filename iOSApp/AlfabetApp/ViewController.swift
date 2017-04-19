2d5c295efa3cc09d21f525b05d43a3f24b3aa58e
OhYeah356 <vrzal.ja@gmail.com>
2017-04-20 00:18:08 +0200

    Xcode project init

---
+++ iOSApp/AlfabetApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AlfabetApp
+//
+//  Created by Jan Vrzal on 20.04.17.
+//  Copyright © 2017 Jan Vrzal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
